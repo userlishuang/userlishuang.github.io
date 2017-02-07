80bd723ae589ac25fad394e023482057a0514c7a
userlishuang <2091401510@qq.com>
2017-02-07 08:58:07 +0800

    profile

--- profile/js/click.js
+++ profile/js/click.js
@@ -1,46 +1,46 @@
  $('.txt1').click(function(event) {
-     $('.content').fadeOut(1000);
+     $('.myprofile').fadeOut(1000);
      $('.aboutMe').fadeIn(1000);
  });
  $('.txt2').click(function(event) {
-     $('.content').fadeOut(1000);
+     $('.myprofile').fadeOut(1000);
      $('.show').fadeIn(1000);
      //只有进入页面才执行定时器图片轮播
      timer = setInterval(change, 2000);
  });
  $('.txt3').click(function(event) {
-     $('.content').fadeOut(1000);
+     $('.myprofile').fadeOut(1000);
      $('.skill').fadeIn(1000);
  });
  $('.txt4').click(function(event) {
-     $('.content').fadeOut(1000);
+     $('.myprofile').fadeOut(1000);
      $('.personal').fadeIn(1000);
  });
  $('.txt5').click(function(event) {
-     $('.content').fadeOut(1000);
+     $('.myprofile').fadeOut(1000);
      $('.contact').fadeIn(1000);
  });
  $('.aboutMe .txt').click(function(event) {
      $('.aboutMe').fadeOut(1000);
-     $('.content').fadeIn(1000);
+     $('.myprofile').fadeIn(1000);
  });
  $('.show .txt').click(function(event) {
      $('.show').fadeOut(1000);
-     $('.content').fadeIn(1000);
+     $('.myprofile').fadeIn(1000);
      // 退出页面停掉定时器
      clearInterval(timer);
  });
  $('.skill .txt').click(function(event) {
      $('.skill').fadeOut(1000);
-     $('.content').fadeIn(1000);
+     $('.myprofile').fadeIn(1000);
  });
  $('.personal .txt').click(function(event) {
      $('.personal').fadeOut(1000);
-     $('.content').fadeIn(1000);
+     $('.myprofile').fadeIn(1000);
  });
  $('.contact .txt').click(function(event) {
      $('.contact').fadeOut(1000);
-     $('.content').fadeIn(1000);
+     $('.myprofile').fadeIn(1000);
  });
  $('.first-col4-1').click(function() {
      $('.col4-2 p').eq(0).removeClass('active').siblings().addClass('active');
