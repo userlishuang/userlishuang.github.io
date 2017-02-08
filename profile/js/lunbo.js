95e211a46b03eeb76bfa0710d54af4736d9f240b
userlishuang <2091401510@qq.com>
2017-02-08 09:08:32 +0800

    2.8

--- profile/js/lunbo.js
+++ profile/js/lunbo.js
@@ -3,7 +3,7 @@ clearInterval(timer);
 var index = 3;
 
 function change() {
-    $('.col2-2 img').eq(index).css({ zIndex: '1' }).siblings().css({ zIndex: '0' });
+    $('.col2-2 a').eq(index).css({ zIndex: '1' }).siblings().css({ zIndex: '0' });
     $('#leftImg').css({ top: 115 * (3 - index) + 'px' });
     index--;
     if (index < 0) {
